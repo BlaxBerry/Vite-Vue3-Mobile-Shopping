2a607703c1ca7a9d0520742a2809e346458d4af0
Chen <chenjiaxu333@gmail.com>
2021-04-13 01:15:20 +0900

    项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+
+    this is HomePage
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import XXX from ""
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
