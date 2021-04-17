de46f6932a887126f461308b044ca0ed2941c0cb
Chen <chenjiaxu333@gmail.com>
2021-04-18 03:33:28 +0900

    商品详情的Tips小提示组件

---
+++ src/components/Tips.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <!-- Tipt小提示组件 -->
+        <van-row>
+            <van-col span="8"><van-icon name="checked" color="green"/>30天无条件退货</van-col>
+            <van-col span="8"><van-icon name="checked" color="green"/>48小时快速退款</van-col>
+            <van-col span="8"><van-icon name="checked" color="green"/>满88元立刻包邮</van-col>
+        </van-row>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.van-row {
+    height: 0.30rem;
+    line-height: 0.30rem;
+    .van-icon {
+        transform: translate(-0.02rem, 0.02rem);
+    }
+}
+</style>
--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -2,18 +2,40 @@
     <div>
         <!-- 商品详情 -->
         <!-- 商品id：{{$route.query.id}} -->
+
+        <!-- banner VantUI轮播图 -->
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+            <van-swipe-item v-for="item in bannerSwipeGallery" :key="item.id">
+                <img 
+                    :src="item.img_url" 
+                    :alt="item.img_desc" 
+                    width="100%" 
+                    style="display"
+                >
+            </van-swipe-item>
+        </van-swipe>
+        
+        <!-- 提示组件 -->
+        <Tips></Tips>
+
     </div>
 </template>
 
 <script>
 // 引入接口请求 api
 import {GetGoodDetailData} from "@/request/api.js"
+// 引入 Tips小提示组件
+import Tips from "@/components/Tips.vue"
 
 export default {
     data(){
         return {
-
+            // 轮播图数组
+            bannerSwipeGallery:[]
         }
+    },
+    components:{
+        Tips
     },
     created(){
         // 发送请求，获得该商品的详情
@@ -22,6 +44,9 @@ export default {
             id:this.$route.query.id
         }).then(result=>{
             console.log(result.data.data);
+            // 赋值轮播图数组
+            this.bannerSwipeGallery = result.data.data.gallery;
+
         })
     }
 }
