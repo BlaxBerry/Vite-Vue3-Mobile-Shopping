0857675f22b38ca4b4b1f06a326bfa7dbc68378e
Chen <chenjiaxu333@gmail.com>
2021-04-18 05:17:57 +0900

    Tips小提示组件修改

--- src/components/Tips.vue
+++ src/components/Tips.vue
@@ -2,9 +2,9 @@
     <div>
         <!-- Tipt小提示组件 -->
         <van-row>
-            <van-col span="8"><van-icon name="checked" color="green"/>30天无条件退货</van-col>
-            <van-col span="8"><van-icon name="checked" color="green"/>48小时快速退款</van-col>
-            <van-col span="8"><van-icon name="checked" color="green"/>满88元立刻包邮</van-col>
+            <van-col span="8"><van-icon name="checked" color="green"/>30天无忧退货</van-col>
+            <van-col span="8"><van-icon name="checked" color="green"/>48小时速退款</van-col>
+            <van-col span="8"><van-icon name="checked" color="green"/>满88元立包邮</van-col>
         </van-row>
 
 
@@ -21,6 +21,7 @@ export default {
 .van-row {
     height: 0.30rem;
     line-height: 0.30rem;
+    font-size: 0.05rem;
     .van-icon {
         transform: translate(-0.02rem, 0.02rem);
     }
